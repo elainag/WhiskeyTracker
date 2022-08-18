89d6e7291bd5be5162d9a89725115424e3a8721e
elainag <elaina.gillon@gmail.com>
2022-08-18 09:38:07 +0100

    distillery repo/controller updated

--- src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -1,9 +1,11 @@
 package com.codeclan.example.WhiskyTracker;
 
 import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -14,17 +16,19 @@ import static org.junit.Assert.assertEquals;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class WhiskyTrackerApplicationTests {
-
+	@Autowired
 	WhiskyRepository whiskyRepository;
+	@Autowired
+	DistilleryRepository distilleryRepository;
 
 	@Test
 	public void contextLoads() {
 	}
 
-//	@Test
-//	public void canGetWhiskeyByYear(){
-//		List<Whisky> whiskeyYear = whiskyRepository.findByYear(1995);
-//		assertEquals(1,whiskeyYear.size());
-//
-//	}
+	@Test
+	public void canGetWhiskeyByYear(){
+		List<Whisky> whiskeyYear = whiskyRepository.findByYear((1995));
+		assertEquals(1,whiskeyYear.size());
+
+	}
 }
